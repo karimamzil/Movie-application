a43bd948c9e15d80bff0707d27540babdee048f4
karim amzil <karimamzil19@gmail.com>
2020-07-25 22:42:32 +0100

    ajout des fonctionalités d'affichage d'un résultat

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-function Result(props) {
-    return (
-        <div className="result">
-            <img src={props.result.Poster} alt=""/>
-    <h3>{props.result.Title}</h3>
-            
-        </div>
-    )
+function Result({ result, openPopup }) {
+	return (
+		<div className="result" onClick={() => openPopup(result.imdbID)}>
+			<img src={result.Poster} />
+			<h3>{result.Title}</h3>
+		</div>
+	)
 }
 
 export default Result
