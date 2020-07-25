df246ed86364c4257288e0caa41e45051cc7de5d
karim amzil <karimamzil19@gmail.com>
2020-07-25 22:39:36 +0100

    modification de le component search

--- src/App.js
+++ src/App.js
@@ -1,39 +1,70 @@
-import React ,{useState } from 'react';
-import logo from './logo.svg';
-import './index.css';
+import React, { useState } from 'react'
 import axios from 'axios'
+
 import Search from './components/Search'
 import Results from './components/Results'
+import Popup from './components/Popup'
+
 function App() {
-  const [state,setState]=useState({
-    s:"",
-    results:[],
-    selected:{}
-  })
-  const url="http://www.omdbapi.com/?i=tt3896198&apikey=e3afc99f"
-  const search=(e)=>{
-    if(e.key==="Enter"){
-      axios(url+"&s"+state.s).then((data)=>{
-        let results=data.search;
-      setState(prevState=>{return{...prevState,results:results}})})
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      });
     }
   }
-  const handleInput=(e)=>{
-    let s=e.target.value;
-    setState(prevState=>{return {...prevState,s: s}})
-  console.log(state.s);
+  
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s: s }
+    });
   }
+
+  const openPopup = id => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
+  }
+
   return (
     <div className="App">
       <header>
-      <h1>Movies</h1>
+        <h1>Movie Database</h1>
       </header>
       <main>
-      <Search handleInput={handleInput} search={search}/>
-      <Results results={state.results}/>
+        <Search handleInput={handleInput} search={search} />
+
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
       </main>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-function Search(props) {
-    return (
-        <section className="searchbox-wrap">
-        <input type="text" placeholder="Search for a movie..." className="searchbox" onChange={props.handleInput}/>
-        </section>
-    )
+function Search ({ handleInput, search }) {
+	return (
+		<section className="searchbox-wrap">
+			<input 
+				type="text" 
+				placeholder="Search for a movie..." 
+				className="searchbox" 
+				onChange={handleInput}
+				onKeyPress={search}
+			/>
+		</section>
+	)
 }
 
 export default Search
